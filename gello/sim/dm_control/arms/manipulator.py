cf17f31d964b19e771d64e86b3c9f24643fee40d
jeonchanwook <eddie3618@knu.ac.kr>
2026-09-01 13:26:52 +0900

    refactor(sim): deduplicate attach_hand_to_arm into gello/sim/utils
    Co-authored-by: redEddie <78861908+redEddie@users.noreply.github.com>

--- gello/sim/dm_control/arms/manipulator.py
+++ gello/sim/dm_control/arms/manipulator.py
@@ -11,46 +11,7 @@ from dm_control.mujoco.wrapper import mjbindings
 from dm_control.suite.utils.randomizers import random_limited_quaternion
 
 from gello.sim.dm_control import mjcf_utils
-
-
-def attach_hand_to_arm(
-    arm_mjcf: mjcf.RootElement,
-    hand_mjcf: mjcf.RootElement,
-    # attach_site: str,
-) -> None:
-    """Attaches a hand to an arm.
-
-    The arm must have a site named "attachment_site".
-
-    Taken from https://github.com/deepmind/mujoco_menagerie/blob/main/FAQ.md#how-do-i-attach-a-hand-to-an-arm
-
-    Args:
-      arm_mjcf: The mjcf.RootElement of the arm.
-      hand_mjcf: The mjcf.RootElement of the hand.
-      attach_site: The name of the site to attach the hand to.
-
-    Raises:
-      ValueError: If the arm does not have a site named "attachment_site".
-    """
-    physics = mjcf.Physics.from_mjcf_model(hand_mjcf)
-
-    # attachment_site = arm_mjcf.find("site", attach_site)
-    attachment_site = arm_mjcf.find("site", "attachment_site")
-    if attachment_site is None:
-        raise ValueError("No attachment site found in the arm model.")
-
-    # Expand the ctrl and qpos keyframes to account for the new hand DoFs.
-    arm_key = arm_mjcf.find("key", "home")
-    if arm_key is not None:
-        hand_key = hand_mjcf.find("key", "home")
-        if hand_key is None:
-            arm_key.ctrl = np.concatenate([arm_key.ctrl, np.zeros(physics.model.nu)])
-            arm_key.qpos = np.concatenate([arm_key.qpos, np.zeros(physics.model.nq)])
-        else:
-            arm_key.ctrl = np.concatenate([arm_key.ctrl, hand_key.ctrl])
-            arm_key.qpos = np.concatenate([arm_key.qpos, hand_key.qpos])
-
-    attachment_site.attach(hand_mjcf)
+from gello.sim.utils import attach_hand_to_arm
 
 
 class Manipulator(composer.Entity, abc.ABC):
--- gello/sim/sim_robot.py
+++ gello/sim/sim_robot.py
@@ -10,45 +10,9 @@ import zmq
 from dm_control import mjcf
 
 from gello.core.robot import Robot
+from gello.sim.utils import attach_hand_to_arm
 
 assert mujoco.viewer is mujoco.viewer
-
-
-def attach_hand_to_arm(
-    arm_mjcf: mjcf.RootElement,
-    hand_mjcf: mjcf.RootElement,
-) -> None:
-    """Attaches a hand to an arm.
-
-    The arm must have a site named "attachment_site".
-
-    Taken from https://github.com/deepmind/mujoco_menagerie/blob/main/FAQ.md#how-do-i-attach-a-hand-to-an-arm
-
-    Args:
-      arm_mjcf: The mjcf.RootElement of the arm.
-      hand_mjcf: The mjcf.RootElement of the hand.
-
-    Raises:
-      ValueError: If the arm does not have a site named "attachment_site".
-    """
-    physics = mjcf.Physics.from_mjcf_model(hand_mjcf)
-
-    attachment_site = arm_mjcf.find("site", "attachment_site")
-    if attachment_site is None:
-        raise ValueError("No attachment site found in the arm model.")
-
-    # Expand the ctrl and qpos keyframes to account for the new hand DoFs.
-    arm_key = arm_mjcf.find("key", "home")
-    if arm_key is not None:
-        hand_key = hand_mjcf.find("key", "home")
-        if hand_key is None:
-            arm_key.ctrl = np.concatenate([arm_key.ctrl, np.zeros(physics.model.nu)])
-            arm_key.qpos = np.concatenate([arm_key.qpos, np.zeros(physics.model.nq)])
-        else:
-            arm_key.ctrl = np.concatenate([arm_key.ctrl, hand_key.ctrl])
-            arm_key.qpos = np.concatenate([arm_key.qpos, hand_key.qpos])
-
-    attachment_site.attach(hand_mjcf)
 
 
 def build_scene(
